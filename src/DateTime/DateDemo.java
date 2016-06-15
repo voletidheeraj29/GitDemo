b88f332de2249548282cd6ae107972bd3800a6fb
unknown <aishwaryav@aishwaryav.cybage.com>
2016-06-15 11:46:12 +0530

    Initial commit

---
+++ src/DateTime/DateDemo.java
@@ -0,0 +1,12 @@
+package DateTime;
+import java.util.Date;
+
+public class DateDemo {
+   public static void main(String args[]) {
+       
+       Date date = new Date();
+        
+       
+       System.out.println(date.toString());
+   }
+}
