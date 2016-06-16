0e2814b96531b93f225a55766482a65ebe0199de
unknown <aishwaryav@aishwaryav.cybage.com>
2016-06-16 15:22:17 +0530

    Commit changes

--- src/DateTime/DateDemo.java
+++ src/DateTime/DateDemo.java
@@ -1,14 +1,13 @@
 package DateTime;
 import java.util.Date;
-import java.text.SimpleDateFormat;
-
+  
 public class DateDemo {
-public static void main(String args[]){
- Date date = new Date();
- 
-  SimpleDateFormat ft =new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
-  System.out.println(date);
-        System.out.println("Current Date: " + ft.format(date));
-}
+   public static void main(String args[]) {
+       // Instantiate a Date object
+       Date date = new Date();
+        
+       // display time and date using toString()
+       System.out.println(date.toString());
+   }
 }
    
